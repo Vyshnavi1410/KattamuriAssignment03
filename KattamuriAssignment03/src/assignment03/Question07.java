a91ce27674e5182802ce5f1cac4f439bde7a5ba8
Vyshnavi1410 <69514165+Vyshnavi1410@users.noreply.github.com>
2023-04-16 14:12:34 -0500

    Add files via upload

---
+++ KattamuriAssignment03/src/assignment03/Question07.java
@@ -0,0 +1,19 @@
+package assignment03;
+
+public class Question07 {
+	int a;
+	int b;
+	/**
+	 * @param a
+	 * @param b
+	 */
+	final Question07(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+	public static void main(String[] args) {
+		Question07 obj = new Question07(9, 3);
+	}
+}
---
+++ KattamuriAssignment03/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author S554978
+ *
+ */
+module KattamuriAssignment03 {
+}
